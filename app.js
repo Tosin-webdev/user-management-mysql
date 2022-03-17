8874f3523dcec8c9bb4f7d4906babf7ccce5d93b
Tosin <oladejit3@gmail.com>
2022-03-17 12:52:17 +0100

    update file

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const exphbs = require("express-handlebars"); // updated to 6.0.X
-const bodyParser = require("body-parser");
 const mysql = require("mysql");
-
-require("dotenv").config();
+const routes = require("./server/routes/user");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+require("dotenv").config({ path: ".env" });
 
 // Parsing middleware
 // Parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 // Parse application/json
-// app.use(bodyParser.json());
 app.use(express.json()); // New
 
 // To serve Static Files (img, css files)
@@ -25,24 +24,35 @@ app.engine(".hbs", handlebars.engine);
 app.set("view engine", ".hbs");
 
 // Connection Pool
-// You don't need the connection here as we have it in userController
-let connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
 
-connection.getConnection((err, connection) => {
-  if (err) throw err;
-  console.log("Connected as ID " + connection.threadId);
-});
-
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-const routes = require("./server/routes/user");
 app.use("/", routes);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// let connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+// });
+
+// app.get("/", function (req, res) {
+//   connection.query(
+//     'SELECT * FROM usermanagement WHERE status = "active"',
+//     (err, rows) => {
+//       // When done with the connection, release it
+//       if (!err) {
+//         let removedUser = req.query.removed;
+//         res.render("home", { rows, removedUser });
+//       } else {
+//         console.log(err);
+//       }
+//       console.log("The data from user table: \n", rows);
+//     }
+//   );
+// });
+
+// connection.getConnection((err, connection) => {
+//   if (err) throw err;
+//   console.log("Connected as ID " + connection.threadId);
+// });
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,15 +1,32 @@
 const mysql = require("mysql");
+require("dotenv").config({ path: ".env" });
 
-let connection = mysql.createPool({
+let connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
 
+console.log(1);
 // View users
 exports.view = (req, res) => {
-  res.render("home");
-
-  //   use the connection
+  // res.render("home");
+  console.log(5);
+  // // User the connection
+  connection.query(
+    'SELECT * FROM usermanagement WHERE status = "active"',
+    (err, rows) => {
+      // When done with the connection, release it
+      if (!err) {
+        let removedUser = req.query.removed;
+        res.render("home", { rows, removedUser });
+      } else {
+        console.log(err);
+      }
+      console.log("The data from user table: \n", rows);
+    }
+  );
 };
+
+// module.exports =
